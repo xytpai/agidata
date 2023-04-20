3b59669f5040a3fe97c7150e725d0aa5c5b01b46
Toy Xu <xytpai@foxmail.com>
2023-04-20 12:25:15 +0800

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -4,26 +4,22 @@ read -p "Are you sure to migrate? (Y/N):" para
 case $para in
 [yY])
 
-smalldataset=(LCCC squad20 qingyun hc3-chinese glm-chinese colossalchat math_qa)
-for item in ${smalldataset[*]}; do
-CURDIR=${item}
-OUTDIR=${DST}/${CURDIR}
-echo "${CURDIR} -> ${OUTDIR}"
-mkdir -p ${OUTDIR}
-cp ${CURDIR}/*.py ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.json ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.jsonl ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.parquet ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.zip ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.csv ${OUTDIR} 2>/dev/null
-cp ${CURDIR}/*.tar ${OUTDIR} 2>/dev/null
-cd ${OUTDIR}
-python check.py
-cd -
-done
+datasets=(
+LCCC
+squad20
+qingyun
+hc3-chinese
+glm-chinese
+colossalchat
+math_qa
+chinese_chatgpt_corpus
+belle
+wiki_zh
+baike
+web_qa
+)
 
-mediumdataset=(chinese_chatgpt_corpus belle wiki_zh baike web_qa)
-for item in ${mediumdataset[*]}; do
+for item in ${datasets[*]}; do
 CURDIR=${item}
 OUTDIR=${DST}/${CURDIR}
 echo "${CURDIR} -> ${OUTDIR}"
