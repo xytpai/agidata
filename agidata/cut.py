8d5f9cc26bf98f6a9db6435512fb25543b298ca7
xytpai <xytpai@foxmail.com>
2023-04-20 06:55:08 +0000

    add strip

--- agidata/cut.py
+++ agidata/cut.py
@@ -2,6 +2,7 @@ import jieba
 
 
 def process_text(text):
+    text = text.strip()
     out = [item.lower() for item in jieba.cut(text) if item != ' ']
     out = ' '.join(out)
     out = out.replace('\n', '[SEP]')
