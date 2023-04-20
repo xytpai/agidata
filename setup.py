f507115ab9bec95192af81187778e18488803535
Toy Xu <xytpai@foxmail.com>
2023-04-20 14:24:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ subprocess.check_call(['pip install -r requirements.txt'], shell=True)
 
 
 setup(
-    name='datasets',
+    name='agidata',
     version='0.0.0',
     packages=['cut'],
     include_package_data=True,
