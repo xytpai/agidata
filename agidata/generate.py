89abbc003911ff133848154a580f543abec29a59
xytpai <xytpai@foxmail.com>
2023-05-06 06:22:59 +0000

    update parallel gen

--- agidata/generate.py
+++ agidata/generate.py
@@ -1,6 +1,7 @@
 import random
 import jieba
 from tqdm import tqdm
+from multiprocessing.pool import ThreadPool
 
 
 def process_text(text: str):
@@ -11,19 +12,28 @@ def process_text(text: str):
     return out
 
 
-def gen_question_answer(fname: str, datas: list):
+def gen_question_answer(fname: str, datas: list, parallel=False):
     print('gen_question_answer ' + fname)
-    pbar = tqdm(total=len(datas))
     lines = []
-    for data in datas:
-        q = process_text(data['question'])
-        a = process_text(data['answer'])
-        line = '[BOS0] ' + q + ' [EOS] [BOS1] ' + a + ' [EOS]\n'
-        lines.append(line)
-        pbar.update(1)
+    if not parallel:
+        pbar = tqdm(total=len(datas))
+        for data in datas:
+            q = process_text(data['question'])
+            a = process_text(data['answer'])
+            line = '[BOS0] ' + q + ' [EOS] [BOS1] ' + a + ' [EOS]\n'
+            lines.append(line)
+            pbar.update(1)
+        pbar.close()
+    else:
+        def gen(data):
+            q = process_text(data['question'])
+            a = process_text(data['answer'])
+            line = '[BOS0] ' + q + ' [EOS] [BOS1] ' + a + ' [EOS]\n'
+            return line
+        p = ThreadPool(32)
+        lines = p.map(datas, gen)
     random.shuffle(lines)
     lines = ''.join(lines)
-    pbar.close()
     with open(fname, 'w') as f:
         f.write(lines)
     return
