e579dffbbb5b9c1b0c6b5db330eb669ec9bab27b
xytpai <xytpai@foxmail.com>
2023-05-13 03:52:48 +0000

    add en specific gen

--- agidata/generate.py
+++ agidata/generate.py
@@ -4,30 +4,35 @@ from tqdm import tqdm
 from multiprocessing.pool import ThreadPool
 
 
-def process_text(text: str):
+def process_text(text: str, language: str):
     text = text.strip()
-    out = [item.lower() for item in jieba.cut(text) if item != ' ']
-    out = ' '.join(out)
+    if language == 'all':
+        out = [item.lower() for item in jieba.cut(text) if item != ' ']
+        out = ' '.join(out)
+    elif language == 'en':
+        out = text.lower()
+    else:
+        raise NotImplementedError('all/en')
     out = out.replace('\n', '[SEP]')
     return out
 
 
-def gen_question_answer(fname: str, datas: list, parallel=False):
+def gen_question_answer(fname: str, datas: list, language='all', parallel=False):
     print('gen_question_answer ' + fname)
     lines = []
     if not parallel:
         pbar = tqdm(total=len(datas))
         for data in datas:
-            q = process_text(data['question'])
-            a = process_text(data['answer'])
+            q = process_text(data['question'], language)
+            a = process_text(data['answer'], language)
             line = '[BOS0] ' + q + ' [EOS] [BOS1] ' + a + ' [EOS]\n'
             lines.append(line)
             pbar.update(1)
         pbar.close()
     else:
         def gen(data):
-            q = process_text(data['question'])
-            a = process_text(data['answer'])
+            q = process_text(data['question'], language)
+            a = process_text(data['answer'], language)
             line = '[BOS0] ' + q + ' [EOS] [BOS1] ' + a + ' [EOS]\n'
             return line
         p = ThreadPool(32)
